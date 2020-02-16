95e2df28bc960f855fc651515c6e8c956f5c73f4
karna <karna29.linux@gmail.com>
2020-02-15 23:48:51 -0700

    test

---
+++ myfirst java/src/karna.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+	
+class text {
+	public static void main (String args[]) {
+		
+		Scanner textjav = new Scanner(System.in);
+		
+		double x;
+		double y;
+		double z;
+		
+		System.out.println("enter your number: ");
+		x = input.nextDouble();
+		
+		
+		
+	}
+	
+	
+}
